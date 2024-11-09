dd6cabfc151819253bcc61e91c9ee6826b78da1d
andrewzw <yapzhewei2002@gmail.com>
2024-11-09 18:57:42 +1100

    clean up

--- Automated_Currency_Converter.py
+++ Automated_Currency_Converter.py
@@ -44,32 +44,3 @@ while True:
     del data[f"{user_currency}"]
     for currency, value in data.items():
         print(f"{currency} : {value * user_amount}")
-
-# GUI
-import tkinter
-
-tkinter._test()
-ctk.set_appearance_mode("dark")
-ctk.set_default_color_theme("dark-blue")
-root = ctk.CTk()
-root.geometry("400x240")
-
-frame = ctk.CTkFrame(master=root)
-frame.pack(pady=20, padx=60, fill="both", expand=True)
-
-label = ctk.CTkLabel(master=frame, text="Currency Converter", font=("Arial", 24))
-label.pack(pady=12, padx=10)
-
-entry1 = ctk.CTkEntry(
-    master=frame,
-    placeholder_text=f"\nChoose from: {CURRENCIES}\nEnter the base currency: ",
-)
-entry1.pack(pady=12, padx=10)
-
-entry2 = ctk.CTkEntry(master=frame, placeholder_text="Enter the amount")
-entry2.pack(pady=12, padx=10)
-
-button = ctk.CTkButton(master=frame, text="Convert", command=convert_currency)
-button.pack(pady=12, padx=10)
-
-root.mainloop()
